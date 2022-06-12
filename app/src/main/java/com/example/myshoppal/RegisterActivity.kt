1522f457f913aeaa217f108d6c85747405c42c3c
hrc-pipo <107201394+hrc-pipo@users.noreply.github.com>
2022-06-12 20:18:16 +0100

    First step of validating the RegisterActivity

--- app/src/main/java/com/example/myshoppal/RegisterActivity.kt
+++ app/src/main/java/com/example/myshoppal/RegisterActivity.kt
@@ -3,16 +3,20 @@ package com.example.myshoppal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toolbar
+import androidx.appcompat.widget.AppCompatCheckBox
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         val tvLogin = findViewById<TextView>(R.id.tv_login)
+        val register = findViewById<Button>(R.id.btn_register)
 
 
         window.setFlags(
@@ -24,13 +28,15 @@ class RegisterActivity : AppCompatActivity() {
 
         tvLogin.setOnClickListener {
             // Do some work here
-
-
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
 
             startActivity(intent)
 
             finish()
+        }
+
+        register.setOnClickListener(){
+            validateRegisterDetails()
         }
 
     }
@@ -51,6 +57,59 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+
+    private fun validateRegisterDetails(): Boolean {
+        val firstName = findViewById<TextView>(R.id.et_first_name)
+        val lastName = findViewById<TextView>(R.id.et_last_name)
+        val email = findViewById<TextView>(R.id.et_email)
+        val password = findViewById<TextView>(R.id.et_password)
+        val confirmPassword = findViewById<TextView>(R.id.et_confirm_password)
+        val terms = findViewById<AppCompatCheckBox>(R.id.cb_terms_and_condition)
+
+
+        return when {
+            TextUtils.isEmpty(firstName.text.toString().trim { it <= ' '}) || firstName.length() < 3 -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(lastName.text.toString().trim { it <= ' '}) || lastName.length() <= 3 -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(email.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(password.text.toString().trim { it <= ' '}) || password.length() < 8 -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(confirmPassword.text.toString().trim { it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            password.text.toString().trim { it <= ' '} != confirmPassword.text.toString().trim { it <= ' '} -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+            }
+
+            !terms.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+
+            else -> {
+                showErrorSnackBar(  "Thanks for registering!", false)
+                true
+            }
         }
     }
 
